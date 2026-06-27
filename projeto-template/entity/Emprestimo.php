f5390aaea2bdd4512e2440d585af1afd9d150913
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-26 23:08:40 -0300

    Emprestimo

---
+++ projeto-template/entity/Emprestimo.php
@@ -0,0 +1,11 @@
+<?php> 
+class Emprestimo {
+
+    private int    $id_emprestimo;
+    private string $status;
+    private string $data;
+    private string $data_devolucao;
+    private int $id_usuario
+    private int $id_livro
+    
+}
