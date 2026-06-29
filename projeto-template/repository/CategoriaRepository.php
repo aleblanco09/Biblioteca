a5da158d9ae61343c30cd727fe2666ec367cc91d
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 04:39:26 -0300

    Update CategoriaRepository.php

--- projeto-template/repository/CategoriaRepository.php
+++ projeto-template/repository/CategoriaRepository.php
@@ -6,8 +6,19 @@ require_once __DIR__ . '/../entity/Categoria.php';
 class CategoriaRepository {
 
     private PDO $pdo;
-
     public function __construct() {
         $this->pdo = getConexao();
+    }
+
+    public function listarCategorias(): array {
+        $stmt = $this->pdo->prepare(
+            'SELECT * FROM categoria'
+        );
+        $stmt->execute();
+        $lista = [];
+        foreach ($stmt->fetchAll() as $dados) {
+            $lista[] = new Pokemon($dados);
+        }
+        return $lista;
     }
 }
