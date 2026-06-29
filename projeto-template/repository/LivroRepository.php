dcff2f711f5996070928fbec1dbf58fe08d8013b
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 01:37:13 -0300

    Update LivroRepository.php

--- projeto-template/repository/LivroRepository.php
+++ projeto-template/repository/LivroRepository.php
@@ -4,12 +4,13 @@ require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../entity/Livro.php';
 
 class LivroRepository {
-    $pdo = new PDO("mysql:host=localhost;dbname=biblioteca_bd");
-      @return Livro[]
-    public function listarLivros(int $IdLivros): array {
-        $stmt = $this->pdo->prepare(
-            'SELECT * FROM livro WHERE id_livro = :$lid ORDER BY nome ASC'
-        );
+    private PDO $pdo;
+    public function __construct() {
+        $this->pdo = getConexao();
+    }
+
+    public function listarLivros(): array {
+        $stmt = $this->pdo->prepare('SELECT * FROM livro');
         $stmt->execute([':lid' => $Id]Livros);
         $lista = [];
         foreach ($stmt->fetchAll() as $dados) {
