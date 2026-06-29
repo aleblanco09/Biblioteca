f3bef447c3ba32ba7887be5f18bb2f7068ab61e8
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 00:48:44 -0300

    Update Usuario.php

--- projeto-template/entity/Usuario.php
+++ projeto-template/entity/Usuario.php
@@ -5,15 +5,17 @@ class Usuario {
     private int    $id;
     private string $nome;
     private string $senha;
+    private string $email;
 
     public function __construct(array $dados) {
         $this->id = (int) ($dados['id'] ?? 0);
         $this->nome = $dados['nome'] ?? '';
         $this->senha = $dados['senha'] ?? '';
-        
+        $this->email = $dados['email'] ?? '';
     }
 
     public function getId():       int    { return $this->id; }
     public function getNome():     string { return $this->nome; }
     public function getSenha():    string { return $this->senha; }
+    public function getEmail():    string { return $this->email; }
 }
