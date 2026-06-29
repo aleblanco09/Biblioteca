d442ec98f3f7d88f6cb4536a16303481c4494992
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 04:45:22 -0300

    Update Pertence.php

--- projeto-template/entity/Pertence.php
+++ projeto-template/entity/Pertence.php
@@ -1,10 +1,11 @@
 <?php
 class Pertence{
-    private int $id_livro;
-    private int $id_categoria;
+    private int $idLivro;
+    private int $idCategoria;
 
     public function __construct(array $dados)
     {
-        
+        $this->idLivro = (int) ($dados['id_livro'] ?? 0);
+        $this->idCategoria = (int) ($dados['id_categoria'] ?? '');
     }
 }
