f7b7fe9d5201cb495d0d07f7a4185ecaefa2af5b
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 05:34:33 -0300

    Update index.php

--- projeto-template/pages/index.php
+++ projeto-template/pages/index.php
@@ -5,18 +5,32 @@ require_once __DIR__ . '/../repository/LivroRepository.php';
 
 $repo = new LivroRepository();
 $livros = $repo->listarLivros();
-
+$repo3 = new CategoriaRepository();
+$categorias = $repo3->listarCategorias();
+$categ=""
 ?>
 
   <h2>Livros</h2>
     <?php foreach ($livros as $livro): ?>
           
-            <?php $repo2 = new PertenceRepository(); $pertencimentos= $repo2->listarPertencimentos(getId);?>
+            <?php $repo2 = new PertenceRepository(); 
+            $pertencimentos= $repo2->listarPertencimentos($livro->getIdLivro());
+            foreach ($pertencimentos as $pertencimento)
+            {
+              foreach ($categorias as $categoria)
+                {
+                 if($categoria === $pertencimento)
+                  {
+                    $categ= $categoria->getNomeCategoria()." ";
+                  }
+                }
+            }?>
             <?php echo "Capa:<br>"?>
             <img src="<?=$livro->getCapa()?>">
             <?php echo "<br>Id:".$livro->getIdLivro()."<br>"?>
             <?php echo "Nome:".$livro->getNomeLivro()."<br>"?>
             <?php echo "Autor:".$livro->getNomeAutor()."<br>"?>
+            <?php echo "Categorias: ".$categ."<br>";
             <a href="livro_edit.php?id=<?= $livro->getIdLivro() ?>">Editar</a>
             <a href="livro_delete.php?id=<?= $livro->getIdLivro() ?>">Excluir</a>
             <a href="emprestar.php?id=<?= $livro->getIdLivro() ?>">Empréstimo</a>
