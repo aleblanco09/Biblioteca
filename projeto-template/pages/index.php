89ccf15935cb4bee84a4d26242a8263bd75599e3
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 06:36:26 -0300

    Update index.php

--- projeto-template/pages/index.php
+++ projeto-template/pages/index.php
@@ -5,6 +5,9 @@ require_once __DIR__ . '/../repository/LivroRepository.php';
 require_once __DIR__ . '/../repository/CategoriaRepository.php';
 require_once __DIR__ . '/../repository/PertenceRepository.php';
 
+$nomeUser = $_SESSION['usuario_nome'];
+echo "<h1>".$nomeUser."</h1>";
+<a href="../pages/logout.php">Sair</a>
 $repo = new LivroRepository();
 $livros = $repo->listarLivros();
 $repo3 = new CategoriaRepository();
@@ -23,16 +26,16 @@ $categ=""
                 {
                  if($categoria->getIdCategoria() === $pertencimento->getIdCategoriaP())
                   {
-                    $categ= $categoria->getNomeCategoria()." ";
+                    $categ= $categ.$categoria->getNomeCategoria()." ";
                   }
                 }
             }?>
-            <?php echo "Capa:<br>"?>
-            <img src="<?=$livro->getCapa()?>">
+            <?php echo "<br>Capa:<br>"?>
+            <img src="<?=$livro->getCapa()?>" width="160">
             <?php echo "<br>Id:".$livro->getIdLivro()."<br>"?>
             <?php echo "Nome:".$livro->getNomeLivro()."<br>"?>
             <?php echo "Autor:".$livro->getNomeAutor()."<br>"?>
-            <?php echo "Categorias: ".$categ."<br>";?>
+            <?php echo "Categorias: ".$categ."<br>";$categ=""?>
             <a href="livro_edit.php?id=<?= $livro->getIdLivro() ?>">Editar</a>
             <a href="livro_delete.php?id=<?= $livro->getIdLivro() ?>">Excluir</a>
             <a href="emprestar.php?id=<?= $livro->getIdLivro() ?>">Empréstimo</a>
