95a0af93704b6328ed870e4e0cf98b2852680902
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 04:28:11 -0300

    Update Categoria.php

--- projeto-template/entity/Categoria.php
+++ projeto-template/entity/Categoria.php
@@ -1,4 +1,12 @@
 <?php class Categoria {
-    private int    $id_categoria;
-    private string $nome_categoria;
+    private int    $id;
+    private string $nome;
+    
+    public function __construct(array $dados) {
+        $this->id = (int) ($dados['id_categoria'] ?? 0);
+        $this->nome = $dados['nome_categoria'] ?? '';
+    }
+
+    public function getId(): int { return $this->id; }
+    public function getNome(): string { return $this->nome; }
 }
