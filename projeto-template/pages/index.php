5eeb920d18dccde3f91c8ef3f0f92f2c1fbca9e7
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-29 01:56:02 -0300

    Exibindo livros

--- projeto-template/pages/index.php
+++ projeto-template/pages/index.php
@@ -9,19 +9,17 @@ $livros = $repo->listarLivros();
 ?>
 
   <h2>Livros</h2>
-  
-  
-        <?php foreach ($pokemons as $pokemon): ?>
-          <tr>
-            <td><?= $pokemon->getId() ?></td>
-            <td><strong><?= htmlspecialchars($pokemon->getNome()) ?></strong></td>
+    <?php foreach ($pokemons as $pokemon): ?>
+          
+            <?=echo "Id:".$livro->getId() ?>
+            <<?= htmlspecialchars($pokemon->getNome()) ?>
             <td><span class="badge badge-tipo"><?= htmlspecialchars($pokemon->getTipo()) ?></span></td>
             <td>Lv. <?= $pokemon->getNivel() ?></td>
             <td class="acoes">
               <a href="pokemon_edit.php?id=<?= $pokemon->getId() ?>" class="btn btn-sm btn-editar">Editar</a>
               <a href="pokemon_delete.php?id=<?= $pokemon->getId() ?>" class="btn btn-sm btn-excluir">Excluir</a>
             </td>
-          </tr>
+          
         <?php endforeach; ?>
       </tbody>
     </table>
