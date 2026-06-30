0a2e161d541ae9620ce1d96c5ffae0b5d7e91d6d
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-30 17:02:35 -0300

    Update livro_delete.php

--- projeto-template/pages/livro_delete.php
+++ projeto-template/pages/livro_delete.php
@@ -2,3 +2,12 @@
 require_once __DIR__.'auth.php';
 require_once __DIR__.'../repository/LivroRepository.php';
 
+$repo = new LivroRepository();
+$id = 0;
+if (isset($_GET['id'])) {
+    $id = (int) $_GET['id'];
+}
+
+if ($id > 0) {
+    $livro = $livro->ProcurarId($id);
+}
