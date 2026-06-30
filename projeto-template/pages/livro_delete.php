f1e2820043ec9719627570378ebfc64c6a957770
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-30 17:21:56 -0300

    Update livro_delete.php

--- projeto-template/pages/livro_delete.php
+++ projeto-template/pages/livro_delete.php
@@ -11,3 +11,20 @@ if (isset($_GET['id'])) {
 if ($id > 0) {
     $livro = $livro->ProcurarId($id);
 }
+
+if (isset($livro)) {
+    header('Location: index.php');
+    exit;
+}?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Confirmar exclusão</title>
+</head>
+<body>
+  
+</body>
+</html>
