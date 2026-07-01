4785fe5a7306472b609264f11ab44c76641b7444
aleblanco09 <stephanoalejandro313@gmail.com>
2026-07-01 18:04:39 -0300

    Update Livro.php

--- projeto-template/entity/Livro.php
+++ projeto-template/entity/Livro.php
@@ -7,21 +7,20 @@
     private string $comentario;
 
     public function __construct(array $dados) {
-        $this->id = (int) ($dados['id_livro']       ?? 0);
-        $this->nome =  $dados['nome_livro']      ?? '';
-        $this->capa = $dados['capa']     ?? '';
+        $this->id = (int) ($dados['id_livro'] ?? 0);
+        $this->nome =  $dados['nome_livro'] ?? '';
+        $this->capa = $dados['capa'] ?? '';
         $this->autor = $dados['nome_autor'] ?? '';
-        $this->comentario = $dados['comentario'] ?? '';
     }
 
     public function getIdLivro(): int { return $this->id; }
     public function getNomeLivro(): string { return $this->nome; }
     public function getCapa(): string { return $this->capa; }
     public function getNomeAutor(): string { return $this->autor; }
-    public function getComentario(): string { return $this->comentario; }
 
     public static function novoLivro(string $nome, string $autor, string $capa): Livro {
-        $livro = new Livro();
+        
+        $livro=new Livro();
         $livro->alterarDados($nome,$autor,$capa);
         return $livro;
     }
