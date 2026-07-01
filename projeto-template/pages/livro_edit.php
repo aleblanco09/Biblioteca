7b10b0edacc377925a6af2f22efb70c0279560a5
aleblanco09 <stephanoalejandro313@gmail.com>
2026-06-30 21:07:05 -0300

    Update livro_edit.php

--- projeto-template/pages/livro_edit.php
+++ projeto-template/pages/livro_edit.php
@@ -4,3 +4,14 @@ require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../repository/LivroRepository.php';
 
 $repo = new LivroRepository();
+
+$id = 0;
+if (isset($_GET['id'])) {
+    $id = (int) $_GET['id'];
+}
+
+$livro = null;
+if ($id > 0) {
+    $livro = $repo->procurarId($id);
+}
+
