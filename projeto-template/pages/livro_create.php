7944af1ca7b11dfb4bdd961bc42516c6efbdffac
aleblanco09 <stephanoalejandro313@gmail.com>
2026-07-01 15:05:31 -0300

    Update livro_create.php

--- projeto-template/pages/livro_create.php
+++ projeto-template/pages/livro_create.php
@@ -22,6 +22,13 @@ $diretorio = "../uploads/";
     <title>Novo livro</title>
 </head>
 <body>
-    
+    <h2>Editar livro</h2>
+  <a href="index.php">Voltar</a>
+  <?php if ($erro !== ''): ?>
+  <p><?=$erro?></p>
+<?php endif; ?>
+    <form method="POST" action="livro_create.php?id=<?=$livro->getIdLivro()?>" enctype="multipart/form-data">
+
+    </form>
 </body>
 </html>
