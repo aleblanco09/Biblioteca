4cb3a939d0867214e271ad4b6e8f73b1d2da223d
aleblanco09 <stephanoalejandro313@gmail.com>
2026-07-01 19:13:22 -0300

    Update emprestar.php

--- projeto-template/pages/emprestar.php
+++ projeto-template/pages/emprestar.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../repository/LivroRepository.php';
 require_once __DIR__ . '/../repository/UsuarioRepository.php';
+require_once __DIR__ . '/../repository/EmprestimoRepository.php';
 
 $repo = new LivroRepository();
 $id = 0;
@@ -10,3 +11,5 @@ if (isset($_GET['id'])) {
     $id = (int) $_GET['id'];
 }
 $livro=$repo->procurarId($id);
+?>
+<div></div>
