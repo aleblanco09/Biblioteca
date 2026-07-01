4098d6bceb3d6d8990b73cad664d93a421fa2c06
aleblanco09 <stephanoalejandro313@gmail.com>
2026-07-01 18:34:07 -0300

    Update Emprestimo.php

--- projeto-template/entity/Emprestimo.php
+++ projeto-template/entity/Emprestimo.php
@@ -16,10 +16,10 @@ class Emprestimo {
         $this->id_usuario = (int) ($dados['id_usuario'] ?? 0);
         $this->id_livro = (int) ($dados['id_livro'] ?? 0);
     }
-    public function getIdEmprestimo():        int    { return $this->id_emprestimo; }
-    public function getStatus():      string { return $this->status; }
-    public function getData():      string { return $this->data; }
-    public function getDataDevolucao():     int    { return $this->data_devolucao; }
-    public function getIdUsuarioE(): int    { return $this->id_usuario; }
-    public function getIdLivroE(): int    { return $this->id_livro; }
+    public function getIdEmprestimo(): int { return $this->id_emprestimo; }
+    public function getStatus(): string { return $this->status; }
+    public function getData(): string { return $this->data; }
+    public function getDataDevolucao(): int { return $this->data_devolucao; }
+    public function getIdUsuarioE(): int { return $this->id_usuario; }
+    public function getIdLivroE(): int { return $this->id_livro; }
 }
