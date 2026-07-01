35caa451f4ae2c71b0b3917fa23a27b805f782ec
aleblanco09 <stephanoalejandro313@gmail.com>
2026-07-01 14:42:41 -0300

    Update livro_create.php

--- projeto-template/pages/livro_create.php
+++ projeto-template/pages/livro_create.php
@@ -1,1 +1,6 @@
+<?php
 
+require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../repository/LivroRepository.php';
+require_once __DIR__ . '/../repository/CategoriaRepository.php';
+require_once __DIR__ . '/../repository/PertenceRepository.php';
